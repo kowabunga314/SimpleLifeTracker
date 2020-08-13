4b4d49cd6bee7deec6e0b91ce309d59e1080557f
kowabunga314 <dougfrost314@gmail.com>
2020-08-13 18:25:36 -0500

    worked around button shadow bug by using layout container as button ¯\_(ツ)_/¯

--- app/src/main/java/com/kaiju/simplelifetracker/PlayerFragment.kt
+++ app/src/main/java/com/kaiju/simplelifetracker/PlayerFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import org.w3c.dom.Text
@@ -41,9 +42,9 @@ class PlayerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val positiveButton = view.findViewById<Button>(R.id.game_button_positive)
+        val positiveButton = view.findViewById<LinearLayout>(R.id.game_container_button_positive)
         val scoreTextView = view.findViewById<TextView>(R.id.game_textview_score)
-        val negativeButton = view.findViewById<Button>(R.id.game_button_negative)
+        val negativeButton = view.findViewById<LinearLayout>(R.id.game_container_button_negative)
 
         positiveButton.setOnClickListener { handleIncrementScore(scoreTextView) }
         positiveButton.setOnLongClickListener {
