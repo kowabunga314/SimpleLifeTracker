59d835a77036c75d5de14f3be3eac4bd71b84d06
kowabunga314 <dougfrost314@gmail.com>
2020-08-15 00:47:23 -0500

    Added support for night mode and specialized night mode files

--- app/src/main/java/com/kaiju/simplelifetracker/GameFragment.kt
+++ app/src/main/java/com/kaiju/simplelifetracker/GameFragment.kt
@@ -5,11 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebViewFragment
 import android.widget.Button
-import android.widget.Toast
 import androidx.fragment.app.FragmentManager
-import kotlinx.android.synthetic.main.fragment_game.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,10 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [GameFragment2.newInstance] factory method to
+ * Use the [GameFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class GameFragment2 : Fragment() {
+class GameFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -67,7 +64,7 @@ class GameFragment2 : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            GameFragment2().apply {
+            GameFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
