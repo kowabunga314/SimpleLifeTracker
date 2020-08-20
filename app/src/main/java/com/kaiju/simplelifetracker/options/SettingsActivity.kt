90936a742c87e9b2762afcc7a4aa47857bc1fa72
kowabunga314 <dougfrost314@gmail.com>
2020-08-20 00:02:25 -0500

    removed extra code that wasn't doing anything

--- app/src/main/java/com/kaiju/simplelifetracker/options/SettingsActivity.kt
+++ app/src/main/java/com/kaiju/simplelifetracker/options/SettingsActivity.kt
@@ -21,55 +21,7 @@ class SettingsActivity : AppCompatActivity() {
 
     }
 
-    companion object {
-        /**
-         * preference value change listener that updates preference to reflect summary's new value
-         */
-
-        private val sBindPreferenceSummaryToValueListener = Preference.OnPreferenceChangeListener { preference, value ->
-
-            val stringValue = value.toString()
-
-            if (preference is ListPreference) {
-                // Get correct display value in entries list
-                val index = preference.findIndexOfValue(stringValue)
-
-                preference.setSummary(
-                    if (index >= 0)
-                        preference.entries[index]
-                    else
-                        null
-                )
-            } else {
-                // For other types just set summary to value's string representation
-                preference.summary = stringValue
-            }
-
-            true
-
-        }
-
-        private fun bindPreferenceSummaryToValue(preference: Preference) {
-            // Set listener to watch for value changes
-            preference.onPreferenceChangeListener = sBindPreferenceSummaryToValueListener
-
-            // Trigger listener immediately with pref's current value
-            sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-            PreferenceManager
-                .getDefaultSharedPreferences(preference.context)
-                .getString(preference.key, ""))
-        }
-    }
-
     class SettingsFragment : PreferenceFragmentCompat() {
-
-//        override fun onCreate(savedInstanceState: Bundle?) {
-//            super.onCreate(savedInstanceState)
-//            addPreferencesFromResource(R.xml.root_preferences)
-//
-////            findPreference<Preference>("display_mode")?.let { bindPreferenceSummaryToValue(it) }
-////            findPreference<Preference>("starting_life")?.let { bindPreferenceSummaryToValue(it) }
-//        }
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
