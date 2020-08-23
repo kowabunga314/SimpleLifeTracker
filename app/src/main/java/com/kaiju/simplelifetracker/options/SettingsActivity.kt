50df43700b6dbbde16bd41efa7ce3587e06658fd
kowabunga314 <dougfrost314@gmail.com>
2020-08-22 19:28:06 -0500

    handler for list preference, not working yet

--- app/src/main/java/com/kaiju/simplelifetracker/options/SettingsActivity.kt
+++ app/src/main/java/com/kaiju/simplelifetracker/options/SettingsActivity.kt
@@ -1,18 +1,12 @@
 package com.kaiju.simplelifetracker.options
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.InputType
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.*
 import com.kaiju.simplelifetracker.R
-import com.kaiju.simplelifetracker.game.GameActivity
-import com.kaiju.simplelifetracker.game.GameFragment
-import kotlinx.android.synthetic.main.game_fragment_game.*
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -25,8 +19,25 @@ class SettingsActivity : AppCompatActivity() {
             .replace(R.id.settings, SettingsFragment())
             .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        actionBar?.show()
 
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .registerOnSharedPreferenceChangeListener(sharedPreferences)
+
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        val darkModeString = getString(R.string.key_display_mode)
+        key?.let {
+            if (it == darkModeString) sharedPreferences?.let { pref ->
+                val darkModeValues = resources.getStringArray(R.array.display_values)
+                when (pref.getString(darkModeString, darkModeValues[0])) {
+                    darkModeValues[0] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    darkModeValues[1] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    darkModeValues[2] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+            }
+        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
