0dabc297061d8d5859dd3ce9c14adb39cf754cf6
kowabunga314 <dougfrost314@gmail.com>
2020-08-22 19:48:13 -0500

    CHOOSE YOUR DISPLAY MODE

--- app/src/main/java/com/kaiju/simplelifetracker/options/SettingsActivity.kt
+++ app/src/main/java/com/kaiju/simplelifetracker/options/SettingsActivity.kt
@@ -20,24 +20,6 @@ class SettingsActivity : AppCompatActivity() {
             .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        PreferenceManager.getDefaultSharedPreferences(this)
-            .registerOnSharedPreferenceChangeListener(sharedPreferences)
-
-    }
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        val darkModeString = getString(R.string.key_display_mode)
-        key?.let {
-            if (it == darkModeString) sharedPreferences?.let { pref ->
-                val darkModeValues = resources.getStringArray(R.array.display_values)
-                when (pref.getString(darkModeString, darkModeValues[0])) {
-                    darkModeValues[0] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-                    darkModeValues[1] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                    darkModeValues[2] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                }
-            }
-        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
@@ -61,6 +43,25 @@ class SettingsActivity : AppCompatActivity() {
                 }
 
                 activity?.finish()
+
+                true
+            }
+
+            // Set theme preference
+            val listPreference = findPreference<ListPreference>(getString(R.string.key_display_mode))
+            listPreference?.setOnPreferenceChangeListener { preference, newValue ->
+
+                when (newValue) {
+                    "light" -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    }
+                    "dark" -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }
+                    "system_default" -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    }
+                }
 
                 true
             }
