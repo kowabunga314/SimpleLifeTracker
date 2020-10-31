6ef141fa90aaf4cd44a1c84bf6e0b42e1bb386ab
kowabunga314 <dougfrost314@gmail.com>
2020-10-30 23:38:47 -0500

    populating adapter

--- app/src/main/java/com/kaiju/simplelifetracker/game/PlayerAdapter.kt
+++ app/src/main/java/com/kaiju/simplelifetracker/game/PlayerAdapter.kt
@@ -1,5 +1,13 @@
 package com.kaiju.simplelifetracker.game
 
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+
 // Class to act as adapter in recyclerview
-class PlayerAdapter {
+class PlayerAdapter: Adapter {
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+    //
 }
