f3bebcc31fa75796a2eca2de5b6da6beb97c4154
kowabunga314 <dougfrost314@gmail.com>
2020-11-04 22:53:57 -0600

    added member functions for player adapter class

--- app/src/main/java/com/kaiju/simplelifetracker/game/PlayerAdapter.kt
+++ app/src/main/java/com/kaiju/simplelifetracker/game/PlayerAdapter.kt
@@ -1,15 +1,52 @@
 package com.kaiju.simplelifetracker.game
 
+import android.database.DataSetObserver
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Adapter
 
 // Class to act as adapter in recyclerview
 class PlayerAdapter: Adapter {
+    override fun registerDataSetObserver(p0: DataSetObserver?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun unregisterDataSetObserver(p0: DataSetObserver?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(p0: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun hasStableIds(): Boolean {
+        TODO("Not yet implemented")
+    }
+
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
         TODO("Not yet implemented")
         // Wire up buttons
         // Wire up text views
+    }
+
+    override fun getItemViewType(p0: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getViewTypeCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun isEmpty(): Boolean {
+        TODO("Not yet implemented")
     }
     //
 }
